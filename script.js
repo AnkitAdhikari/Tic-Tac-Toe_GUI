055ddb4015dbd672fe3277a5384f767ccaad692b
AnkitAdhikari <ankitadhikari31512@gmail.com>
2024-02-20 22:16:13 +0545

    add player array update, winning condition and winning model

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 const fields2 = document.querySelectorAll('.field');
 console.log(fields2)
 
+const model = document.querySelector('.model');
+
 const player1 = [];
 
 const player2 = [];
+
+let winnerName = '';
 
 const winningCombination = [
   [1, 2, 3],
@@ -23,6 +27,13 @@ fields2.forEach((field) => {
   field.addEventListener('click', (e) => {
     if (e.target.textContent == '') {
       e.target.textContent = activePlayer == player1 ? 'O' : 'X';
+      const choice = +e.target.dataset.value
+      updatePlayerChoice(choice);
+      const won = checkWinningCondition();
+      if (won) {
+        updateWinnerName();
+        displayWinningMessage();
+      }
       changePlayer();
     }
 
@@ -37,7 +48,29 @@ function changePlayer() {
   }
 }
 
+function updatePlayerChoice(choice) {
+  activePlayer.push(choice);
+}
+
+function checkWinningCondition() {
+  return winningCombination.some(combination => combination.every(el => activePlayer.includes(el)));
+}
+
+function updateWinnerName() {
+  winnerName = activePlayer == player1 ? 'O' : 'X';
+  document.querySelector('.winnerName').textContent = winnerName;
+}
+
+function displayWinningMessage() {
+  model.classList.toggle('hidden');
+}
+
 // TODO
-// get clicked field's value and update palyer array
-// check winning condition after the palyer array updates
-// Display winning message 
+// get clicked field's value and update palyer array (#0f0)
+// check winning condition after the palyer array updates (#0f0)
+// Display winning message (#0f0)
+// Start new game after game over
+// prompt to input players name
+// display winning name
+// change palyer name afte game over
+// improve model ui
